81182c3a1473392556a62a49b9e669785c3c8466
srhanselman <s.r.hanselman@gmail.com>
2015-05-11 16:14:29 +0200

    Rudimentary Verlet for non-linear hammer drop

--- vibration.py
+++ vibration.py
@@ -29,9 +29,15 @@ class String:
 
   def time_evolution(self):
     F = 0
+    init_velocity = 1.0
     hammer_length = 0.01
     hammer_position = 0.15*L
-    Ms = 1
+    hammer_displacement = 0.0
+    hammer_displacement_minus_n = -init_velocity*delta_t
+    hammer_mass = 1.0
+    K = 1.0
+    p = 2.0
+    Ms = 1.0
     g = np.zeros((N),dtype = float)
     n = 0
     beginwindow = np.floor((hammer_position-hammer_length/2)*N/L)
@@ -40,10 +46,12 @@ class String:
     for i in range(beginwindow,endwindow):
         g[i] = 1.0/(endwindow-beginwindow)
     
+    F = K/hammer_mass * np.abs(hammer_displacement - self.y)**p    
     for i in range(2,N-2):
         self.y_plus_n[i] = self.a_1*self.y[i] + self.a_2*self.y_minus_n[i] + self.a_3*(self.y[i+1]+self.y[i-1])]
         + self.a_4*(self.y[i+2]+self.y[i-2]) + self.a_5*(self.y_minus_n[i+1]+self.y_minus_n[i-1]+self.y_minus_2n[i])
         + (self.delta_t**2*self.N*F*g)/Ms
-    F
+    hammer_displacement = 2*hammer_displacement - hammer_displacement_minus_n
+    + F*delta_t**2
     
 
