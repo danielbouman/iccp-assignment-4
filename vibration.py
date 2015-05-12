23a123ba382cbda597fa9116b84b4dd69ec29157
Daniel Bouman <d.bouman1@gmail.com>
2015-05-12 11:12:01 +0200

    minor cleanup

--- vibration.py
+++ vibration.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 class String:
   
   def __init__(self,note='C2',*args):
-    self.duration = 1000    
+    self.duration = 1000
     self.delta_t = 1/(4*44e3)
     # self.delta_t = 0.1
 
@@ -35,8 +35,6 @@ class String:
       self.N = 16
       self.L = 0.09
       self.epsilon = 8.67e-4
-      
-
 
     self.delta_x = self.L/self.N
     kappa_squared = self.epsilon*(self.c**2)*(self.L**2)
@@ -51,7 +49,6 @@ class String:
         raise ValueError("CFL condition broken")
     
     nu = 2*b_2*(self.delta_t)/(self.delta_x**2)
-    
 
     self.a_1 = (-(self.r**2)*mu)/D
     self.a_2 = (self.r**2+4*(self.r**2)*mu+nu)/D
@@ -81,22 +78,10 @@ class String:
     n = 0
     beginwindow = int(np.floor((hammer_position-hammer_length/2)*self.N/self.L))
     endwindow = int(np.ceil((hammer_position+hammer_length/2)*self.N/self.L))
-
-    for i in range(beginwindow,endwindow):
-        g[i] = 1.0/(endwindow-beginwindow)
-
-
-    # self.r = 0.4
-
-    a_10 = self.r**2
-    a_11 = 2-2*self.r**2
-
-    self.xAxis = np.linspace(0,self.L,self.N)
-    # self.y = 0.01*np.sin(2*np.pi*self.xAxis/self.L)
-    # self.y_minus_n = 0.01*np.sin(3*np.pi*self.xAxis/self.L)
+    g[beginwindow:endwindow-1] = 1.0/(endwindow-beginwindow)
 
     for t in range(0,self.duration):
-        # F = K/hammer_mass * np.abs(hammer_displacement - self.y)**p        
+        # F = K/hammer_mass * np.abs(hammer_displacement - self.y)**p
         for i in range(2,self.N-2):
           self.y_plus_n[i] = self.a_1*(self.y[i+2]+self.y[i-2])+self.a_2*(self.y[i+1]+self.y[i-1]) \
           + self.a_3*self.y[i]+self.a_4*self.y_minus_n[i]+self.a_5*(self.y_minus_n[i+1]+self.y_minus_n[i-1]) \
@@ -116,17 +101,13 @@ class String:
 
 
   def animate(self,saveAnimation=False):
-    # Time evolved probability
     fig, ax = plt.subplots()
-    if saveAnimation == True:
-      # Set up formatting for the movie files
-      Writer = animation.writers['ffmpeg']
-      writer = Writer(fps=15, metadata=dict(artist='Bouman and Goodenough'))
-
+    self.xAxis = np.linspace(0,self.L,self.N)
+    
     line, = ax.plot(self.xAxis, np.sin(self.xAxis))
 
     def animate(i):
-        line.set_ydata(self.time_evolved_string[:,10*i])  # update the data
+        line.set_ydata(self.time_evolved_string[:,i])  # update the data
         return line,
 
     #Init only required for blitting to give a clean slate.
@@ -137,14 +118,13 @@ class String:
     ani = animation.FuncAnimation(fig, animate, np.arange(1, self.duration), init_func=init,
         interval=35, blit=True,repeat=False)
     plt.axis([0,self.L,-0.015,0.015])
-    # plt.plot(self.xAxis,ani)
     if saveAnimation == True:
+      # Set up formatting for the movie files
+      Writer = animation.writers['ffmpeg']
+      writer = Writer(fps=15, metadata=dict(artist='Bouman and Goodenough'))
       print("Saving animation...")
-      start = time.time()
-      ani.save(self.potentialName+'.mp4', writer=writer)
-      end = time.time()
-      print(end - start)
-      print("Done. Animation saved as "+self.potentialName+".mp4")
+      ani.save('vibration.mp4', writer=writer)
+      print("Done. Animation saved as vibration.mp4")
     else:
       plt.show()
     return
