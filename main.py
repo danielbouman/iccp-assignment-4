044733a1d08a517238967c879a8ca0e5d2ff3143
Daniel Bouman <d.bouman1@gmail.com>
2015-05-14 17:25:17 +0200

    Made notes.csv more readable

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from vibration import String
 from vibration import Chord
 import numpy as np
 
-string1 = String('x4',2)            # Define string with: note, duration in seconds
+string1 = String('c4',2)            # Define string with: note, duration in seconds
 string1.time_evolution_f90()        # Start time evolution with: damper (optional), damper delay
 # note1 = string1.getWave()         # Get wave when creating a chord
 string1.exportSound('csv_test')     # Save as .wav file with: extra text in filename (optional)
