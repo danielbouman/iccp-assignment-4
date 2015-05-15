bb8e73a494a6dcbbac88a57c40380cc73536f444
Selwyn Hanselman <shanselman@localhost.localdomain>
2015-05-15 16:30:08 +0200

    many wavs

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from vibration import String
 from vibration import Chord
 import numpy as np
 
-string1 = String('c4',10)            # Define string with: note, duration in seconds
+string1 = String('c2',2)            # Define string with: note, duration in seconds
 string1.time_evolution_f90()        # Start time evolution with: damper (optional), damper delay
 # note1 = string1.getWave()         # Get wave when creating a chord
-string1.exportSound('new_hammer_flatterparabola')     # Save as .wav file with: extra text in filename (optional)
+string1.exportSound('new_hammer_par')     # Save as .wav file with: extra text in filename (optional)
 
 # chord = Chord()
 # chord.createChord(note1,note2,note3)
